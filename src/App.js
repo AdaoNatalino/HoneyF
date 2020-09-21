83ddfd9823e7920570690fb99bfbcc51f9f6f965
Adao Natalino <adaojosen@hotmail.com>
2020-09-21 11:06:31 +0100

    context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
 import "./App.css";
 import Authorized from "./Components/Authorized/Authorized";
 import Unauthorized from "./Components/Unauthorized/Unauthorized";
+
+export const UserContext = createContext()
 
 function App() {
   const [user, setUser] = useState(null);
